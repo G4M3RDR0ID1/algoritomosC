e62af6cd59c9ea2416e02c8f9a812ce22b878ac9
G4M3RDR0ID1 <gamerdroid33@hotmail.com>
2024-08-25 23:22:03 -0300

    first commit

---
+++ 1159/1159/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int X = int.Parse(Console.ReadLine());
+
+        while (X != 0) 
+        {
+            if (X % 2 == 0)
+            {
+                X = (X) + (X + 2) + (X + 4) + (X + 6) + (X + 8);
+            }
+            else 
+            {
+                X = (X + 1) + (X + 3) + (X + 5) + (X + 7) + (X + 9);
+            }
+            Console.WriteLine(X);
+            X = int.Parse(Console.ReadLine());
+        }
+    }
+
+}
