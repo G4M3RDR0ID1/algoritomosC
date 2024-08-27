a806f14a743c7cace900c06ff79e4148c288c2fe
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-27 00:04:38 -0300

    Add files via upload

---
+++ Ex02Matriz/Ex02Matriz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Serialization;
+
+string[] valores = Console.ReadLine().Split(' ');
+
+int m = int.Parse(valores[0]);
+int n  = int.Parse(valores[1]);
+
+int[,] matriz = new int[m, n];
+
+for (int i = 0; i < m; i++) 
+{
+    string[] s = Console.ReadLine().Split(); 
+    for (int j = 0; j < n; j++) 
+    {
+        matriz[i,j] = int.Parse(s[j]);
+    }
+}
+
+int[] vet = new int[m];
+
+for (int i = 0;i < m; i++) 
+{
+    int soma = 0;
+    for (int j = 0;j < n; j++) 
+    {
+        soma = soma + matriz[i,j];
+    }
+    vet[i] = soma;
+}
+
+for (int i = 0; i < m; i++) 
+{
+    Console.WriteLine(vet[i]);
+}
