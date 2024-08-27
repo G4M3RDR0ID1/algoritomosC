6bdb9c72301489f7cfed3b3e20962bb42afc8db4
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-27 09:31:02 -0300

    Add files via upload

---
+++ ex01OrientacaoobjClasse/ex01OrientacaoobjClasse/Program.cs
@@ -0,0 +1,43 @@
+﻿using ex01OrientacaoobjClasse;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triganulo X: ");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com as medidas do triganulo Y: ");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Area de x = " + areaX.ToString("F4"));
+            Console.WriteLine("Area de y = " + areaY.ToString("F4"));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior area: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior area: y");
+            }
+        }
+    }
+}
