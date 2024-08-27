175f8e22e44f8e9cb3d97551eb78be3db42d5cfe
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-27 10:03:24 -0300

    Add files via upload

---
+++ ex02Obj/ex02Obj/Funcionarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex02Obj
+{
+    internal class Funcionarios
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ ex02Obj/ex02Obj/Program.cs
@@ -0,0 +1,33 @@
+﻿using ex02Obj;
+using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionarios F1, F2;
+            F1 = new Funcionarios();
+            F2 = new Funcionarios();
+
+            Console.WriteLine("Dados do primeiro funcionario: ");
+            Console.Write("Nome: ");
+            F1.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            F1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionario: ");
+            Console.Write("Nome: ");
+            F2.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            F2.Salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Salario medio = ");
+
+            double SalarioMedio = (F1.Salario + F2.Salario) / 2.00;
+
+            Console.WriteLine(SalarioMedio.ToString("f2"));
+        }
+    }
+}
