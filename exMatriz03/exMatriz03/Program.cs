8693917ac3f802b7584c240bcb2716bb434f5166
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-27 23:25:19 -0300

    Add files via upload

---
+++ exMatriz03/exMatriz03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+int n = int.Parse(Console.ReadLine());
+
+int[,] mat = new int [n, n];
+
+for (int i = 0; i < n; i++) 
+{
+    string[] s = Console.ReadLine().Split(' ');
+
+    for (int j = 0; j < n; j++) 
+    {
+        mat[i,j] = int.Parse(s[j]);
+    }
+}
+
+
+for (int i = 0;i < n; i++) 
+{
+    int maior = mat[i,0];
+    for (int j = 1;j < n; j++) 
+    {
+        if (maior < mat[i,j]) 
+        {
+            maior = mat[i,j];
+        }
+    }
+    Console.WriteLine(maior);
+}
