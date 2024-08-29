e7a0e593dac5900106a06ae8931a29c98e133b38
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-29 10:26:20 -0300

    Add files via upload

---
+++ Retangulo_CLasse/Retangulo_CLasse/Program.cs
@@ -0,0 +1,14 @@
+﻿using Retangulo_CLasse;
+
+Console.WriteLine("Entre a largura e altura do retangulo: ");
+
+Retangulo d = new Retangulo();
+
+
+
+d.Largura = double.Parse(Console.ReadLine());
+d.Altura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Area = " + d.Area().ToString("f2"));
+Console.WriteLine("Perimetro = " + d.Perimetro().ToString("f2"));
+Console.WriteLine("Diagonal = " + d.Diagonal().ToString("f2"));
---
+++ Retangulo_CLasse/Retangulo_CLasse/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo_CLasse
+{
+    internal class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area() 
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal() 
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+    }
+}
