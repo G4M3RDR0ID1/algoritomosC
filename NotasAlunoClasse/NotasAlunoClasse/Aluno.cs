b54514ecb36437927e74baf2fce347a3f942c2d4
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-29 23:46:31 -0300

    Add files via upload

---
+++ NotasAlunoClasse/NotasAlunoClasse/Aluno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotasAlunoClasse
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double N1;
+        public double N2;
+        public double N3;
+
+        public double NotaFinal() 
+        {
+            return N1 + N2 + N3;
+        }
+
+        public bool Aprovado() 
+        {
+            if (NotaFinal() >= 60) 
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.00;
+            }
+            else 
+            {
+                return 60 - NotaFinal();
+            }
+        }
+
+    }
+}
---
+++ NotasAlunoClasse/NotasAlunoClasse/Program.cs
@@ -0,0 +1,27 @@
+﻿using NotasAlunoClasse;
+
+Aluno dados = new Aluno();
+
+Console.Write("Nome do aluno: ");
+dados.Nome = Console.ReadLine();
+Console.WriteLine("Digite as tres notas do aluno: ");
+dados.N1 = double.Parse(Console.ReadLine());
+dados.N2 = double.Parse(Console.ReadLine());
+dados.N3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Nota Final: " + dados.NotaFinal().ToString("f2"));
+
+if (dados.Aprovado()) 
+{
+    Console.WriteLine("APROVADO");
+}
+else 
+{
+    Console.WriteLine("REPROVADO");
+    Console.WriteLine("FALTARAM " + dados.NotaRestante().ToString("f2"));
+}
+
+
+
+
+
