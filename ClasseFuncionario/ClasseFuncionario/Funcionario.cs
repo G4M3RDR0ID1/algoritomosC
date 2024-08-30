560bb19e6963b04eaeadd894330a2514da3c3434
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-29 23:05:07 -0300

    Add files via upload

---
+++ ClasseFuncionario/ClasseFuncionario/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseFuncionario
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+
+        public double SalarioLiquido() 
+        {
+            return Salario - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem) 
+        {
+            Salario = Salario + (Salario * porcentagem / 100.0);
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", R$ "
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ ClasseFuncionario/ClasseFuncionario/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClasseFuncionario;
+
+Funcionario dados = new Funcionario();
+
+Console.Write("Nome: ");
+dados.Nome = Console.ReadLine();
+Console.Write("Salario bruto: ");
+dados.Salario = double.Parse(Console.ReadLine());
+Console.Write("Imposto: ");
+dados.Imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Funcionario : " + dados);
+
+Console.Write("Digite a porcentagem para aumentar o salario: ");
+double porcentagem = double.Parse(Console.ReadLine());
+dados.AumentarSalario(porcentagem);
+
+Console.WriteLine("Dados atualizados: " + dados);
