36b2b77e5600c282e150a2fe641c466decfd9b6f
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-31 00:01:52 -0300

    Add files via upload

---
+++ 1173/1173/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int[] vet = new int[10];
+        int n = int.Parse(Console.ReadLine());
+        vet[0] = n;
+        
+        for (int i = 1; i < 10; i++)
+        {
+            vet[i] = n * 2;
+            n = n * 2;
+        }
+
+        for (int i = 0; i < 10; i++) 
+        {
+            Console.WriteLine("N[" + i + "] = " + vet[i]);
+        }
+
+    }
+
+}
