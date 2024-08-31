21b84d3e6404eb200832b6b11c58c81b8c136f17
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-30 23:26:23 -0300

    Add files via upload

---
+++ 1164/1164/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int SomaDeNumerosPerfeitos = 0;
+
+            for (int j = 1; j < numero; j++)
+            {
+                if (numero % j == 0)
+                {
+                    SomaDeNumerosPerfeitos = SomaDeNumerosPerfeitos + j;
+                }
+            }
+
+            if (SomaDeNumerosPerfeitos == numero)
+            {
+                Console.WriteLine(numero + " eh perfeito");
+            }
+            else
+            {
+                Console.WriteLine(numero + " nao eh perfeito");
+            }
+
+        }
+
+    }
+
+}
