da86db1019f9728cb3de882b14dddde528f6e155
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-30 23:28:30 -0300

    Add files via upload

---
+++ 1075/1075/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+
+
+
+        for (int i = 0; i < n; i++)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int SomaDeNumeros = 0;
+
+            for (int j = 1; j <= numero; j++)
+            {
+                if (numero % j == 0)
+                {
+                    SomaDeNumeros++;
+                }
+            }
+
+            if (SomaDeNumeros == 2)
+            {
+                Console.WriteLine(numero + " eh primo");
+            }
+            else
+            {
+                Console.WriteLine(numero + " nao eh primo");
+            }
+
+        }
+
+    }
+
+}
