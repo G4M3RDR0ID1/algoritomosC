728e8b778e38ba8da17a43d90258d5df56c856db
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-08-30 23:29:44 -0300

    Add files via upload

---
+++ 1172/1172/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int[] vet = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            vet[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (vet[i] <= 0)
+            {
+                vet[i] = 1;
+            }
+
+            Console.WriteLine("X[" + i + "] = " + vet[i]);
+        }
+
+    }
+
+}
