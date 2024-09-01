7728222115aa65c1a6e56f35101bd6b2b8bbca24
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-01 17:52:53 -0300

    Add files via upload

---
+++ 1174/1174/Program.cs
@@ -0,0 +1,14 @@
+﻿double[] a = new double[100];
+
+for (int i = 0; i < 4; i++)
+{
+    a[i] = double.Parse(Console.ReadLine());
+}
+
+for (int i = 0;i < 4; i++) 
+{
+    if (a[i] <= 10) 
+    {
+        Console.WriteLine("A[" + i + "] = " + a[i]);
+    }
+}
