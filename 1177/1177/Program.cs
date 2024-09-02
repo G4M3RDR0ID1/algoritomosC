845f9abc0a1b72b646ef6c3edf0d5a2aa09e00c3
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-02 14:16:22 -0300

    Add files via upload

---
+++ 1177/1177/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int[] N = new int[1000];
+
+        int T = int.Parse(Console.ReadLine());
+        int sequencia = 0;
+
+        for (int i = 0; i < 1000; i++)
+        {
+            N[i] = sequencia;
+            sequencia++;
+            if (sequencia == T)
+            {
+                sequencia = 0;
+            }
+
+            Console.WriteLine("N[" + i + "] = " + N[i]);
+        }
+
+    }
+
+}
