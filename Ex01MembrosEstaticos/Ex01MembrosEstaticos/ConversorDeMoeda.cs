9c90adc4450275a9538b6eb3bfb70a927b4c128b
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-02 15:11:15 -0300

    Add files via upload

---
+++ Ex01MembrosEstaticos/Ex01MembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01MembrosEstaticos
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double CalculoDeDolares(double cotacao, double quantia) 
+        {
+            double total = cotacao * quantia;
+            return total + total * Iof / 100.00;
+        }
+    }
+}
---
+++ Ex01MembrosEstaticos/Ex01MembrosEstaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using Ex01MembrosEstaticos;
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Qual é a cotaçao do dolar: ");
+        double cotacao = double.Parse(Console.ReadLine());
+        Console.WriteLine("Quantos dólares voce vai comprar? ");
+        double dolares = double.Parse(Console.ReadLine());
+
+        double resultado = ConversorDeMoeda.CalculoDeDolares(cotacao, dolares);
+
+        Console.WriteLine("Valor a ser pago em reais = " + resultado.ToString("f2"));
+
+
+    }
+
+}
