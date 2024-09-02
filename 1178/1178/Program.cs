073393c7419fe768d399570644a9e0b8ca88b86d
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-02 20:46:34 -0300

    Add files via upload

---
+++ 1178/1178/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double[] vet = new double[10];
+        
+        double x = double.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < 10; i++) 
+        {
+            vet[i] = x;
+            x = x / 2;
+            Console.WriteLine("N[" + i + "] = " + vet[i].ToString("F4"));
+        }
+
+    }
+
+}
