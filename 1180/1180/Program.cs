8e125f43ab85bcc624859832069539d910c2b87e
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-03 14:15:26 -0300

    Add files via upload

---
+++ 1180/1180/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+
+        string[] vet = Console.ReadLine().Split(' ');
+
+        int[] x = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            x[i] = int.Parse(vet[i]);
+        }
+
+        int menor = x[0];
+        int menorposicao = 0;
+
+        for (int i = 0; i < n; i++) 
+        {
+
+            if (x[i] < menor) 
+            {
+                menor = x[i];
+                menorposicao = i;
+            }
+        }
+
+        Console.WriteLine("Menor valor: " + menor);
+        Console.WriteLine("Posicao: " + menorposicao);
+
+    }
+
+}
