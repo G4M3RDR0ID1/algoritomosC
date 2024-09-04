0d311a03db8b5e4c8ee0587b6ccb1791aaeab833
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-03 21:29:22 -0300

    Add files via upload

---
+++ 1181/1181/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double[,] mat = new double[12, 12];
+
+        int linha = int.Parse(Console.ReadLine());
+
+        char operacao = char.Parse(Console.ReadLine());
+
+        for (int i = 0; i < 12; i++) 
+        {
+            for (int j = 0; j < 12; j++) 
+            {
+                mat[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        double soma = 0;
+        double media = 0;
+
+        for (int i = 0;i < 12; i++) 
+        {
+            if (operacao == 'S') 
+            {
+                soma += mat[linha,i];
+            }
+            if (operacao == 'M') 
+            {
+                media += mat[linha,i];
+            }
+        }
+
+        media = media / 12.0;
+
+        if (operacao == 'S') 
+        {
+            Console.WriteLine(soma.ToString("f1"));
+        }
+        if (operacao == 'M') 
+        {
+            Console.WriteLine(media.ToString("f1"));
+        }
+    }
+}
