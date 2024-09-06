bda49af4bd94474c59986d6c42cf470da872f55d
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-06 00:06:01 -0300

    Add files via upload

---
+++ 2006/2006/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int cha = int.Parse(Console.ReadLine());
+        string[] valores = Console.ReadLine().Split(' ');
+        int[] votos = new int[5];
+        int acertos = 0;
+        
+        for (int i = 0; i < 5; i++)
+        {
+            votos[i] = int.Parse(valores[i]);
+
+            if (cha == votos[i])
+            {
+                acertos++;
+            }
+        }
+
+        Console.WriteLine(acertos);
+    }
+
+}
