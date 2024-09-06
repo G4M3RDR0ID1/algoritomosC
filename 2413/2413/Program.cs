9501610e7903867aae3ae893cf826a201fa8b9d6
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-06 00:13:01 -0300

    Add files via upload

---
+++ 2413/2413/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int terceiroLink = int.Parse(Console.ReadLine());
+
+        int segundoLink = 2 * terceiroLink;
+
+        int primeiroLink = 2 * segundoLink;
+
+        Console.WriteLine(primeiroLink);
+
+    }
+
+}
