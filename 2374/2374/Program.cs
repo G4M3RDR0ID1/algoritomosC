7e70ae5d93873ed09b014b80d8a6309301c7cc44
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-06 13:34:50 -0300

    Add files via upload

---
+++ 2374/2374/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+
+        int d = n - m;
+
+        Console.WriteLine(d);
+    }
+
+}
