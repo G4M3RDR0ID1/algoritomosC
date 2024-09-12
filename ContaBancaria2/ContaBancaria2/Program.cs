378c1ee8f82cdc7026067975ce9c33bfd6f32c96
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-11 23:57:37 -0300

    Add files via upload

---
+++ ContaBancaria2/ContaBancaria2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ContaBancaria2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaBancaria conta;
+
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Entre o titular da conta: ");
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if (resposta == 's')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double deposito = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numero, titular, deposito);
+            }
+            else 
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.Write(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine());
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine());
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+
+        }
+    }
+}
