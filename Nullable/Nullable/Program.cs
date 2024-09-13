216e222db8d373d506bea8cc4c744136639fe53b
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-12 22:59:24 -0300

    Add files via upload

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.0;
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+        }
+    }
+}
