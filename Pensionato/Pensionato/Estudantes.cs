6802c6ffcaeaaa7d199eb52c1f90b991f62ed193
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-13 16:17:20 -0300

    Add files via upload

---
+++ Pensionato/Pensionato/Estudantes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensionato
+{
+    internal class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudantes(string nome, string email) 
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ Pensionato/Pensionato/Program.cs
@@ -0,0 +1,31 @@
+﻿using Pensionato;
+
+Estudantes[] quartos = new Estudantes[10];
+
+Console.Write("Quantos quartos serão alugados? ");
+int quartosAlugados = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 1; i <= quartosAlugados; i++) 
+{
+    Console.WriteLine("Aluguel #" + i + ":");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+    Console.Write("Quarto: ");
+    int quarto = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    quartos[quarto] = new Estudantes(nome, email);
+}
+
+Console.WriteLine("Quartos ocupados: ");
+
+for (int i = 0; i < 10; i++) 
+{
+    if (quartos[i] != null) 
+    {
+        Console.WriteLine(i + ": " + quartos[i]);
+    }
+}
