4ee4d62e5ca73d483aad9c8453e36fe094032b3d
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-16 22:03:29 -0300

    Add files via upload

---
+++ Listaex01/Listaex01/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listaex01
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public void Salario(double salario) 
+        {
+            Salary = salario;
+        }
+
+        public void increaseSalary(double percentage) 
+        {
+            Salary = percentage / 100 * Salary;
+        }
+    }
+}
---
+++ Listaex01/Listaex01/Program.cs
@@ -0,0 +1,26 @@
+﻿using Listaex01;
+
+Employee funcionario = new Employee();
+List<Employee> lista = new List<Employee>();
+Console.Write("How many employees will be registered?");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 1; i <= n; i++) 
+{
+    Console.WriteLine("Emplyoee #" + i);
+    Console.Write("Id:");
+    funcionario.Id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    funcionario.Name = Console.ReadLine();
+    Console.Write("Salary");
+    double salario = double.Parse(Console.ReadLine());
+    funcionario.Salario(salario);
+    Console.WriteLine();
+    lista.Add(funcionario);
+}
+
+Console.Write("Enter the employee id that will have salary increase : ");
+int idd = int.Parse(Console.ReadLine());
+
+foreach 
