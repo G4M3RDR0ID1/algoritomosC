92596f0ab827d1adcc202d2e7de792f8b4425008
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-17 09:23:23 -0300

    Add files via upload

---
+++ ListaFuncionarios/ListaFuncionarios/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listaex01
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary) 
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        public void Salario(double salario)
+        {
+            Salary = salario;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            Salary += percentage / 100 * Salary;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2");
+        }
+    }
+}
---
+++ ListaFuncionarios/ListaFuncionarios/Program.cs
@@ -0,0 +1,46 @@
+﻿﻿using Listaex01;
+using System.Security.Principal;
+
+List<Employee> lista = new List<Employee>();
+
+Console.Write("How many employees will be registered? ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for (int i = 1; i <= n; i++) 
+{
+    Console.WriteLine("Emplyoee #" + i);
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salary: ");
+    double salario = double.Parse(Console.ReadLine());
+    lista.Add(new Employee(id, name, salario));
+    Console.WriteLine();
+
+}
+
+Console.Write("Enter the employee id that will have salary increase : ");
+int idd = int.Parse(Console.ReadLine());
+
+Employee s1 = lista.Find(x => x.Id == idd);
+
+if (s1 != null) 
+{
+    Console.Write("Enter the percentage: ");
+    double porcentagem = double.Parse(Console.ReadLine());
+    s1.increaseSalary(porcentagem);
+}
+else 
+{
+    Console.WriteLine("This id does not exist!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Updated list of employees: ");
+
+foreach (Employee obj in lista) 
+{
+    Console.WriteLine(obj);
+}
