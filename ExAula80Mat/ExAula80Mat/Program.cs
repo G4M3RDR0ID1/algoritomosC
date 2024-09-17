600e75843d667c24ed9cce6b7b6b261452aed33d
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-17 11:49:25 -0300

    Add files via upload

---
+++ ExAula80Mat/ExAula80Mat/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[,] mat = new int[n,n];
+
+
+for (int i = 0; i < n; i++)
+{
+    string[] s = Console.ReadLine().Split(' ');
+    for (int j = 0; j < n; j++) 
+    {
+        mat[i, j] = int.Parse(s[j]);
+    }
+}
+
+int negativo = 0;
+
+for (int i = 0;i < n; i++) 
+{
+    for(int j = 0;j < n; j++) 
+    {
+        if (i == j) 
+        {
+            Console.Write(mat[i, j] + " ");           
+        }
+
+        if (mat[i, j] < 0) 
+        {
+            negativo++;
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Negativos: " + negativo);
