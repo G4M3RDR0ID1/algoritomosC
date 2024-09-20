43fb057b706847d1f9c124d3a25f4d9f40dc267d
G4M3RDR0ID1 <138540010+G4M3RDR0ID1@users.noreply.github.com>
2024-09-20 09:45:20 -0300

    Add files via upload

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,17 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
+
+string txt = OrderStatus.PendingPayment.ToString();
+Console.WriteLine(txt);
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+Console.WriteLine(os);
